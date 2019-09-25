1283d766d196b8d4840efc46c3e23d2c41db3497
Chris Floyd <christopherfloyd16@gmail.com>
2019-09-25 01:10:01 -0400

    changed color of navbar header text and footer and navbar color

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -1,4 +1,6 @@
-var firstName = prompt("Please enter your first name.");
-var firstChar = firstName.slice(0,1).toUpperCase();
-var restOfChar = firstName.slice(1,20).toLowerCase();
-alert("Hello " + firstChar + restOfChar + ", welcome home!");
+// var firstName = prompt("Please enter your first name.");
+// var firstChar = firstName.slice(0,1).toUpperCase();
+// var restOfChar = firstName.slice(1,20).toLowerCase();
+// restOfChar = restOfChar.toLowerCase();
+// alert("Hello " + firstChar + restOfChar + ", have a look around, then maybe haed on over and join!");
+// getElementByClass('btn')
